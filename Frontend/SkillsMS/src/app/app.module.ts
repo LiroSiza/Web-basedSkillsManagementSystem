8c62d15458d7fc4e9202c286bcdf27fa58c90a53
LiroSiza <cesarreyestorres005@gmail.com>
2025-06-17 21:31:31 -0600

    Components created

--- Frontend/SkillsMS/src/app/app.module.ts
+++ Frontend/SkillsMS/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponentComponent } from './core/navbar-component/navbar-component.component';
+import { ModalSkillsComponentComponent } from './shared/modal-skills-component/modal-skills-component.component';
+import { AuthComponentComponent } from './auth/auth-component/auth-component.component';
+import { CollaboratorsListComponentComponent } from './views/collaborators-list-component/collaborators-list-component.component';
+import { CollaboratorSkillsListComponentComponent } from './views/collaborator-skills-list-component/collaborator-skills-list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent,
+    ModalSkillsComponentComponent,
+    AuthComponentComponent,
+    CollaboratorsListComponentComponent,
+    CollaboratorSkillsListComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/SkillsMS/src/app/views/collaborator-skills-list-component/collaborator-skills-list-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collaborator-skills-list-component',
+  templateUrl: './collaborator-skills-list-component.component.html',
+  styleUrl: './collaborator-skills-list-component.component.scss'
+})
+export class CollaboratorSkillsListComponentComponent {
+
+}
