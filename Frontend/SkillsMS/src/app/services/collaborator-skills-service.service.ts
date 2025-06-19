4181525f2c9058a9f781d7c6d1bdb440e7d6d84b
LiroSiza <cesarreyestorres005@gmail.com>
2025-06-18 19:11:09 -0600

    Alerts Functionality for all the requests

--- Frontend/SkillsMS/src/app/services/collaborator-skills-service.service.ts
+++ Frontend/SkillsMS/src/app/services/collaborator-skills-service.service.ts
@@ -14,10 +14,27 @@ export class CollaboratorSkillsServiceService {
     return this.httpClient.get(_URL_COLLABORATOR_SKILLS + `/${collaboratorId}`);
   }
 
+  // Esta función realiza una solicitud POST para crear una nueva habilidad asociada a un colaborador
   createCollaboratorSkill(data: any): Observable<any> {
     return this.httpClient.post(_URL_COLLABORATOR_SKILLS + `/`, data, {
+      
+      // Se especifica el encabezado Content-Type como application/json
+      // Esto indica que el cuerpo de la petición está en formato JSON
+      // Aunque Angular lo añade automáticamente cuando el body es un objeto, aquí se incluye explícitamente por claridad y compatibilidad
+      headers: { 'Content-Type': 'application/json' }
+
+    });
+  }
+
+  updateCollaboratorSkill(data: any): Observable<any> {
+    return this.httpClient.put(_URL_COLLABORATOR_SKILLS + `/`, data, {
       headers: { 'Content-Type': 'application/json' }
     });
   }
 
+  deleteCollaboratorSkill(id: string, skillName: string): Observable<any> {
+    return this.httpClient.delete(_URL_COLLABORATOR_SKILLS + `/${id}` + `/${skillName}`, {
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
 }
--- Frontend/SkillsMS/src/app/views/collaborator-skills-list-component/collaborator-skills-list-component.component.ts
+++ Frontend/SkillsMS/src/app/views/collaborator-skills-list-component/collaborator-skills-list-component.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CollaboratorSkillsServiceService } from '../../services/collaborator-skills-service.service';
 import { ActivatedRoute, RouterLinkActive } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-collaborator-skills-list-component',
@@ -38,7 +40,7 @@ export class CollaboratorSkillsListComponentComponent {
     this.collaboratorSkillsService.getAllCollaboratorSkills(this.collaboratorData.id).subscribe(
       (data) => {
         this.skills = data[1];
-        console.log('Skills fetched successfully:', this.skills);
+        //console.log('Skills fetched successfully:', this.skills);
       },
       (error) => {
         console.error('Error fetching skills:', error);
@@ -53,33 +55,108 @@ export class CollaboratorSkillsListComponentComponent {
   }
 
   handleSave(skill: any) {
+    const newSkill = {
+      _id : this.collaboratorData.id,
+      strSName : skill.strSName,
+      strSLevel : skill.strSLevel,
+      numSYOE : skill.numSYOE
+    }
     if (this.selectedSkill) {
       // EDITAR
-      console.log('Editando skill:', skill);
-      // Aquí llamas al servicio para actualizar skill (con ID si aplica)
+      //console.log('Editando skill:', skill);
+      this.updateSkill(newSkill);
     } else {
       // NUEVO
-      console.log('Agregando skill:', skill);
-      // Aquí llamas al servicio para crear una nueva skill
-      const newSkill = {
-        _id : this.collaboratorData.id,
-        strSName : skill.strSName,
-        strSLevel : skill.strSLevel,
-        numSYOE : skill.numSYOE
-      }
+      //console.log('Agregando skill:', skill);
       this.createSkill(newSkill);
     }
   }
 
+  deleteCollaboratorSkill(skill: any) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `¿Deseas eliminar la skill "${skill.strSName}"?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#F9A825',
+      cancelButtonColor: '#60C8CB',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.selectedSkill = skill;
+        this.deleteSkill(this.collaboratorData.id, this.selectedSkill.strSName);
+        this.selectedSkill = null;
+      }
+    });
+  }
+
+
   createSkill(newSkillData: any) {
-    console.log(newSkillData);
+    //console.log(newSkillData);
     this.collaboratorSkillsService.createCollaboratorSkill(newSkillData).subscribe(
       (data) => {
-        console.log('New Skill - successfully:', data);
+        //console.log('New Skill - successfully:', data);
         this.fetchCollaborators();
+        Swal.fire(
+          'Agregada',
+          'La skill ha sido agregada correctamente.',
+          'success'
+        );
       },
       (error) => {
         console.error('Error new skill:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'No se creó la skill correctamente.'
+        });
+      }
+    );
+  }
+
+  updateSkill(newSkillData: any) {
+    //console.log(newSkillData);
+    this.collaboratorSkillsService.updateCollaboratorSkill(newSkillData).subscribe(
+      (data) => {
+        //console.log('Updated Skill - successfully:', data);
+        this.fetchCollaborators();
+        Swal.fire(
+          'Actualizada',
+          'La skill ha sido actualizada correctamente.',
+          'success'
+        );
+      },
+      (error) => {
+        console.error('Error update skill:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'No se actualizó la skill correctamente.'
+        });
+      }
+    );
+  }
+
+  deleteSkill(idCollaborator: string, skillName: string) {
+    //console.log("Info delete: ", idCollaborator + skillName);
+    this.collaboratorSkillsService.deleteCollaboratorSkill(idCollaborator, skillName).subscribe(
+      (data) => {
+        //console.log('Deleted Skill - successfully:', data);
+        this.fetchCollaborators();
+        Swal.fire(
+          'Eliminada',
+          'La skill ha sido eliminada correctamente.',
+          'success'
+        );
+      },
+      (error) => {
+        console.error('Error delete skill:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'No se eliminó la skill correctamente.'
+        });
       }
     );
   }
